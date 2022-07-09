39b3382acf79b64d444d819c85c519b7d30a99ce
Kirill <9090144395@mai.ru>
2022-07-09 22:21:05 +0500

    doing

--- lesson_3.py
+++ lesson_3.py
@@ -11,7 +11,7 @@ client = MongoClient('mongodb://localhost:27017/')
 pprint(client.list_database_names())
 
 # Создаём ссылку на базу данных testdb:
-db = client.testdb
+db = client.testdb  # База данных
 
 cars = [ {'name': 'Audi', 'price': 52642},
     {'name': 'Mercedes', 'price': 57127},
@@ -23,6 +23,7 @@ cars = [ {'name': 'Audi', 'price': 52642},
     {'name': 'Volkswagen', 'price': 21600} ]
 
 db.cars.insert_many(cars)
+db.cars # Коллекция
 
 #Теперь посмотрим все коллекции в базе данных с именем testdb:
 print(db.list_collection_names())
@@ -40,9 +41,41 @@ print()
 n_cars = len(list(db.cars.find()))
 print(n_cars)
 
+
+
+fokus_collection = db.cars
+item = fokus_collection.find_one({})
+pprint(item)
+print()
+
+
+for item in fokus_collection.find({'$or':
+                              [
+                                {'name': 'Audi'},
+                                {'price': 21000}
+                               ]
+                            }):
+    pprint(item)
+
+print()
+
+for item in fokus_collection.find({'name': 'Audi'}):
+    pprint(item)
+
+print()
+
+
+
+# for item in persons.find({'age': {'$gte': 29}}):
+#     pprint(item)
+#
+# for item in persons.find({'author': {'$regex': '^Pet$'}}):
+#     pprint(item)
+
+
 # Удалить коллекцию cars из базы данных testdb:
 db.cars.drop()
-
+print('----------------------------')
 
 # 2. Написать функцию, которая производит поиск и выводит на экран вакансии с заработной платой больше введённой суммы
 # (необходимо анализировать оба поля зарплаты). То есть цифра вводится одна, а запрос проверяет оба поля
